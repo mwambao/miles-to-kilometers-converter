5c1a747338938aeb4568a05274b7e81389261c98
Duncan <duncan.owino@gmail.com>
2024-06-20 08:35:28 +0100

    miles to kms converter

---
+++ miles-to-kilometers-converter.js
@@ -0,0 +1,14 @@
+/*
+Create a variable that contains a value in miles, convert it to kilometers, and log the
+value in kilometers in the following format:
+
+The distance of 130 kms is equal to 209.2142 miles
+
+Note: For reference, 1 mile equals 1.60934 kilometers.
+
+ */
+
+let distanceInMiles = 209.2142; // distance in miles
+let distanceInKms = distanceInMiles / 1.60934 ; // convert distance to kms
+
+console.log("The distance of " + distanceInMiles + " miles is equal to " + distanceInKms + " kms");
